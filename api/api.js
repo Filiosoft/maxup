194dbac70e476c4534d01d0d6a503ea0a731d98d
Noah Prail <nprail@filiosoft.com>
2018-02-16 15:11:27 -0500

    Added whoami

--- api/api.js
+++ api/api.js
@@ -1,4 +1,13 @@
 /**
+ * @apiDefine RequireAuth
+ * @apiHeader {String} Authorization Bearer + JWT token.
+ * @apiHeaderExample {json} AuthorizationHeader:
+ *     {
+ *       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
+ *     }
+ */
+
+/**
  * @api {post} /v1/auth Request a login
  * @apiName PostAuth
  * @apiGroup Authentication
@@ -54,6 +63,24 @@
  *
  * @apiExample {curl} Example usage:
  *     curl https://api.s3d.sh/v1/auth/confirm?email=jim@example.com&token=317e0ffc-d77d-489f-b04c-78035a20e6c2
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "message": "Login verified!"
+ *     }
+ */
+
+/**
+ * @api {get} /v1/auth/whoami Who Am I?
+ * @apiName GetWhoami
+ * @apiGroup Authentication
+ * @apiDescription Check the current logged in user. 
+ * @apiUse RequireAuth
+ *
+ * @apiSuccess {String} email       Current users email address.
+ *
+ * @apiExample {curl} Example usage:
+ *     curl https://api.s3d.sh/v1/auth/whoami
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
  *     {
